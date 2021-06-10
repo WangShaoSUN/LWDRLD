090729cae3ad2e546fce2262dca3379d8b621d6f
samwang <samwang@mail.ustc.edu.cn>
2021-06-10 15:33:42 +0800

    Create run1.sh

---
+++ run1.sh
@@ -0,0 +1,18 @@
+CUDA_VISIBLE_DEVICES=1 python main.py --policy "Double_DQN" --env "Zaxxon" --seed 0  --args.lr 0.0001    --exp_name Double_DQN-PongNoFrameskip-v4
+
+CUDA_VISIBLE_DEVICES=1 python main.py --policy "Double_DQN" --env "Zaxxon" --seed 5  --args.lr 0.0002     --exp_name Double_DQN-PongNoFrameskip-v4
+
+CUDA_VISIBLE_DEVICES=1 python main.py --policy "Double_DQN" --env "Zaxxon" --seed 7   --args.lr 0.0003    --exp_name Double_DQN-PongNoFrameskip-v4
+
+CUDA_VISIBLE_DEVICES=1 python main.py --policy "Double_DQN" --env "Zaxxon" --seed 2  --args.lr 0.0004    --exp_name Double_DQN-PongNoFrameskip-v4
+
+CUDA_VISIBLE_DEVICES=1 python main.py --policy "Double_DQN" --env "WizardOfWor" --seed 0  --args.lr 0.0001     --exp_name Double_DQN-PongNoFrameskip-v4
+
+CUDA_VISIBLE_DEVICES=1 python main.py --policy "Double_DQN" --env "WizardOfWor" --seed 1   --args.lr 0.0002    --exp_name Double_DQN-PongNoFrameskip-v4
+
+CUDA_VISIBLE_DEVICES=1 python main.py --policy "Double_DQN" --env "WizardOfWor" --seed 2  --args.lr 0.0003    --exp_name Double_DQN-PongNoFrameskip-v4
+
+CUDA_VISIBLE_DEVICES=1 python main.py --policy "Double_DQN" --env "WizardOfWor" --seed 3  --args.lr 0.0004     --exp_name Double_DQN-PongNoFrameskip-v4
+
+CUDA_VISIBLE_DEVICES=1 python main.py --policy "Double_DQN" --env "Venture" --seed 10   --args.lr 0.0002    --exp_name Double_DQN-PongNoFrameskip-v4
+

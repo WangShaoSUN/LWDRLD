28628e35b7957dcce7721aef9c0261bbaebdae9a
samwang <samwang@mail.ustc.edu.cn>
2021-06-10 23:35:03 +0800

    Update run1.sh

--- run1.sh
+++ run1.sh
@@ -1,18 +1,82 @@
-CUDA_VISIBLE_DEVICES=1 python main.py --policy "Double_DQN" --env "Zaxxon" --seed 0  --lr 0.0001    --exp_name Double_DQN-PongNoFrameskip-v4
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "UpNDown" --seed 0  --lr 0.0001    --exp_name Double_DQN
 
-CUDA_VISIBLE_DEVICES=1 python main.py --policy "Double_DQN" --env "Zaxxon" --seed 5  --lr 0.0002     --exp_name Double_DQN-PongNoFrameskip-v4
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "UpNDown" --seed 5  --lr 0.0002     --exp_name Double_DQN
 
-CUDA_VISIBLE_DEVICES=1 python main.py --policy "Double_DQN" --env "Zaxxon" --seed 7   --lr 0.0003    --exp_name Double_DQN-PongNoFrameskip-v4
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "UpNDown" --seed 7   --lr 0.0003    --exp_name Double_DQN
 
-CUDA_VISIBLE_DEVICES=1 python main.py --policy "Double_DQN" --env "Zaxxon" --seed 2  --lr 0.0004    --exp_name Double_DQN-PongNoFrameskip-v4
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "UpNDown" --seed 2  --lr 0.0004    --exp_name Double_DQN
 
-CUDA_VISIBLE_DEVICES=1 python main.py --policy "Double_DQN" --env "WizardOfWor" --seed 0  --lr 0.0001     --exp_name Double_DQN-PongNoFrameskip-v4
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "Tutankham" --seed 0  --lr 0.0001     --exp_name Double_DQN
 
-CUDA_VISIBLE_DEVICES=1 python main.py --policy "Double_DQN" --env "WizardOfWor" --seed 1   --lr 0.0002    --exp_name Double_DQN-PongNoFrameskip-v4
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "Tutankham" --seed 1   --lr 0.0002    --exp_name Double_DQNdd
 
-CUDA_VISIBLE_DEVICES=1 python main.py --policy "Double_DQN" --env "WizardOfWor" --seed 2  --lr 0.0003    --exp_name Double_DQN-PongNoFrameskip-v4
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "Tutankham" --seed 2  --lr 0.0003    --exp_name Double_DQN
 
-CUDA_VISIBLE_DEVICES=1 python main.py --policy "Double_DQN" --env "WizardOfWor" --seed 3  --lr 0.0004     --exp_name Double_DQN-PongNoFrameskip-v4
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "Tutankham" --seed 3  --lr 0.0004     --exp_name Double_DQN
 
-CUDA_VISIBLE_DEVICES=1 python main.py --policy "Double_DQN" --env "Venture" --seed 10   --lr 0.0002    --exp_name Double_DQN-PongNoFrameskip-v4
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "Tutankham" --seed 10   --lr 0.0006    --exp_name Double_DQN
+
+
+
+
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "TimePilot" --seed 0  --lr 0.0001     --exp_name Double_DQN
+
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "TimePilot" --seed 1   --lr 0.0002    --exp_name Double_DQNdd
+
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "TimePilot" --seed 2  --lr 0.0003    --exp_name Double_DQN
+
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "TimePilot" --seed 3  --lr 0.0004     --exp_name Double_DQN
+
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "TimePilot" --seed 10   --lr 0.0006    --exp_name Double_DQN
+
+
+
+
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "StarGunner" --seed 0  --lr 0.0001     --exp_name Double_DQN
+
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "StarGunner" --seed 1   --lr 0.0002    --exp_name Double_DQNdd
+
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "StarGunner" --seed 2  --lr 0.0003    --exp_name Double_DQN
+
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "StarGunner" --seed 3  --lr 0.0004     --exp_name Double_DQN
+
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "StarGunner" --seed 10   --lr 0.0006    --exp_name Double_DQN
+
+
+
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "YarsRevenge" --seed 0  --lr 0.0001     --exp_name Double_DQN
+
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "YarsRevenge" --seed 1   --lr 0.0002    --exp_name Double_DQNdd
+
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "YarsRevenge" --seed 2  --lr 0.0003    --exp_name Double_DQN
+
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "YarsRevenge" --seed 3  --lr 0.0004     --exp_name Double_DQN
+
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "YarsRevenge" --seed 10   --lr 0.0006    --exp_name Double_DQN
+
+
+
+
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "VideoPinball" --seed 0  --lr 0.0001     --exp_name Double_DQN
+
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "VideoPinball" --seed 1   --lr 0.0002    --exp_name Double_DQNdd
+
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "VideoPinball" --seed 2  --lr 0.0003    --exp_name Double_DQN
+
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "VideoPinball" --seed 3  --lr 0.0004     --exp_name Double_DQN
+
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "VideoPinball" --seed 10   --lr 0.0006    --exp_name Double_DQN
+
+
+
+
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "SpaceInvaders" --seed 0  --lr 0.0001     --exp_name Double_DQN
+
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "SpaceInvaders" --seed 1   --lr 0.0002    --exp_name Double_DQNdd
+
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "SpaceInvaders" --seed 2  --lr 0.0003    --exp_name Double_DQN
+
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "SpaceInvaders" --seed 3  --lr 0.0004     --exp_name Double_DQN
+
+CUDA_VISIBLE_DEVICES=0 python main.py --policy "Double_DQN" --env "SpaceInvaders" --seed 10   --lr 0.0006    --exp_name Double_DQN
 
